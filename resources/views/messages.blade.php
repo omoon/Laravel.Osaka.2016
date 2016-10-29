8730e6214e644be386c0f2ccbe862b2ab10301ee
omoon <omooon@gmail.com>
2016-10-29 14:57:23 +0900

    apply twitter bootstrap

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -4,44 +4,57 @@
     <title>
         Message Board
     </title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
 </head>
 <body>
-<h1>Message Board</h1>
+<div class="container">
 
-@if ($errors->count() > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+    <h1>Message Board</h1>
 
-<form action="say_something" method="post">
+    @if ($errors->count() > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-    {{ csrf_field() }}
+    <form action="say_something" method="post">
 
-    <input type="text" name="message" placeholder="say something">
-    <input type="submit" value="送信">
+        {{ csrf_field() }}
 
-</form>
+        <input class="form-control" type="text" name="message" placeholder="say something">
+        <input class="form-control btn btn-primary" type="submit" value="送信">
 
-{{ $messages->count() }}
+    </form>
 
-<table>
-    <tr>
-        <th>posted at</th>
-        <th>message</th>
-    </tr>
-    @foreach($messages as $message)
-    <tr>
-        <td>{{ $message->created_at }}</td>
-        <td>{{ $message->message }}</td>
-    </tr>
-    @endforeach
-</table>
+    <table class="table table-bordered">
+        <tr>
+            <th>posted at</th>
+            <th>message</th>
+        </tr>
+        @foreach($messages as $message)
+            <tr>
+                <td>{{ $message->created_at }}</td>
+                <td>{{ $message->message }}</td>
+            </tr>
+        @endforeach
+    </table>
 
+</div>
 </body>
 
 
